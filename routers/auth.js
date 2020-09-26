8101f6945ddb08cfc75fb60920133e3fd125826c
tacettinkustu <tacettinkustu>
2020-09-26 12:26:13 +0300

    signIn and signOut functionality

--- routers/auth.js
+++ routers/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const {register,tokentest} = require("../controllers/auth");
-const{getAccessToRoute} = require("../middlewares/authorization/auth")
+const { register, getUser, login , logout } = require("../controllers/auth");
+const { getAccessToRoute } = require("../middlewares/authorization/auth");
 
 const router = express.Router();
 
-router.post("/register",register);
-router.get("/tokentest",getAccessToRoute,tokentest);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/profile", getAccessToRoute, getUser);
+router.get("/logout",getAccessToRoute,logout)
 
 module.exports = router;
